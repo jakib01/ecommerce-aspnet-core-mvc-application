aee05ff18275344d88d29cd82654f6654445ab4b
Julker Nien Akib <julkernienakib@gmail.com>
2022-05-30 00:57:47 +0600

    all models

---
+++ eTickets/Data/MovieCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Data
+{
+    public enum MovieCategory
+    {
+        Action = 1,
+        Comedy,
+        Drama,
+        Documentary
+    }
+}
